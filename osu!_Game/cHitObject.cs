97c3ba0b26cfcf9e00444ff15f9f9791542ca330
Burtpi <hyhu122@gmail.com>
2022-07-02 17:29:34 +0200

    Added base for Sliders

--- osu!_Game/cHitObject.cs
+++ osu!_Game/cHitObject.cs
@@ -5,14 +5,23 @@ namespace osu__Game
     {
         private readonly cApproachCircle mApproachCircle;
         private readonly cCircle mCircle;
+        private readonly cSlider mSlider;
 
         public cHitObject(float aX, float aY, double aTime)
         {
-            mCircle = new cCircle((aX + 192) * 1.7857f, (aY + 96) * 1.7578125f, aTime + 1500);
-            mApproachCircle = new cApproachCircle((aX + 192) * 1.7857f, (aY + 96) * 1.7578125f, aTime + 1500);
+            mCircle = new cCircle((aX + 192) * 1.7857f, (aY + 96) * 1.7578125f, aTime);
+            mApproachCircle = new cApproachCircle((aX + 192) * 1.7857f, (aY + 96) * 1.7578125f, aTime);
             mX = (aX + 192) * 1.7857f;
             mY = (aY + 96) * 1.7578125f;
-            mTime = aTime + 1500;
+            mTime = aTime;
+        }
+        public cHitObject(float aX, float aY, double aTime, float aXEnd, float aYEnd, double aTimeEnd)
+        {
+            mSlider= new cSlider((aX + 192) * 1.7857f, (aY + 96) * 1.7578125f, aTime, (aXEnd + 192) * 1.7857f, (aYEnd + 96) * 1.7578125f, aTimeEnd);
+            mApproachCircle = new cApproachCircle((aX + 192) * 1.7857f, (aY + 96) * 1.7578125f, aTime);
+            mX = (aX + 192) * 1.7857f;
+            mY = (aY + 96) * 1.7578125f;
+            mTime = aTime;
         }
 
         public cHitObject()
---
+++ osu!_Game/cSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace osu__Game
+{
+    public class cSlider : cObject
+    {
+        private float mXEnd { get; set; }
+        private float mYEnd { get; set; }
+        private double mTimeEnd { get; set; }
+
+        public cSlider(float aX, float aY, double aTime, float aXEnd, float aYEnd, double aTimeEnd)
+        {
+            mX = aX;
+            mY = aY;
+            mTime = aTime;
+            mXEnd = aXEnd;
+            mYEnd = aYEnd;
+            mTimeEnd = aTimeEnd;
+            mHover = false;
+        }
+
+    }
+}
